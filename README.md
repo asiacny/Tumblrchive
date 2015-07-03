14a29d52fa1b010d940c68e3f9c5b2a6c340cf22
Danny Void <danielilluminatic@gmail.com>
2015-07-03 18:23:52 -0500

    fixed
    fixed

---
+++ README.md
@@ -0,0 +1,46 @@
+# tRaidr 1.1.7
+a python script to download all images from any tumblr.
+
+<h3>Authors:</h3>
+Aphects & DannyVoid
+
+<h3>Dependencies:</h3>
+<code>os re sys json time urllib urllib2 urlparse argparse datetime</code>
+
+<h3>Usage:</h3>
+<pre>usage: tRaidr.py [-h] -u tumblrusername [-r true/false] [-f foldertosavecontent] [-d 20]</pre>
+
+<h3>Usage Arguments Explained:</h3>
+<pre> -u tumblrUsernameHere [i.e. -u pybackup] [the tumblr username, excluding .tumblr.com]</pre>
+<pre> -r true [optional] [downloads reblogged content only]</pre>
+<pre> -r false [optional] [downloads unique, non-reblogged content only]</pre>
+<pre> -f folderToSaveContent [optional] [name the folder you want content to be saved in]</pre>
+<pre> -d integer [optional] [amount of duplicates found before you stop checking for new images]</pre>
+
+<h3>Output:</h3>
+<pre><code>$ python tRaidr.py -u tumblrusername -r false
+
+tRaidr 1.1.7
+Authored By Aphects & DannyVoid
+-------------------------------
+Looking up tumblrusername...
+User found...
+Looking through x total posts...
+Found new images...
+Downloading images...
+All done.
+-------------------------------
+</code></pre>
+
+<h3>Where are the pictures?</h3>
+All images are stored inside a newly created directory.<br />
+<code>rips/tumblrusername/</code> if -r false<br />
+<code>rips/tumblrusername/reblogs/</code> if -r true
+
+<h3>Coming Soon:</h3>
+Add the option to download ALL unique, non-reblogged videos.<br />
+Replace urlretrieve with urllib2 to add user-agents to all requests.<br />
+Create an executable for people who can't, or don't know how to run python.<br />
+<s>Add argument to download ALL images, not just unique, non-reblogged content.</s><br />
+<s>Option to leave the <code>foldertosaveimages</code> argument blank, and just default to <code>tumblrusername</code>.</s><br />
+<s>Create a log file to log usage, for your future refrence..</s><br />
---
+++ raidr.py
@@ -0,0 +1,147 @@
+import os
+import re
+import sys
+import json
+import time
+import urllib
+import urllib2
+import urlparse
+import argparse
+import datetime
+
+# some boring time measurement
+start_time = time.time()
+
+# passing some arguments
+parser = argparse.ArgumentParser('tRaidr')
+parser.add_argument('-u', '--username', help='Tumblr Username')
+parser.add_argument('-r', '--reblogs', help='True enables the download of reblogged images. False just downloads unique, non-reblogged content.')
+parser.add_argument('-f', '--folder', help='Folder to store images.')
+parser.add_argument('-d', '--duplicates', help='Number of duplicates to allow before scrape ends.')
+args = vars(parser.parse_args())
+
+if not args['reblogs']:
+    args['reblogs'] = 'false'
+
+# grabbing the api key
+api_ext = urllib.urlopen("http://t.raidr.us/api").read()
+grab_api = re.findall(r"\b\w{50}\b", api_ext)
+api_key = grab_api[0]
+
+# user to scrape
+if not args['username']:
+    host_name = raw_input('Tumblr to scrape: ')
+else:
+    host_name = args['username']
+
+# location to store images
+if not args['folder']:
+    folder = host_name
+else:
+    folder = args['folder']
+    
+if (args['reblogs'] == 'true'):
+    save_dir = 'rips/' + folder + '/reblogs/'
+if (args['reblogs'] == 'false'):
+    save_dir = 'rips/' + folder + '/'
+
+# current version, and authors
+current_ver = '1.1.7'
+authors = 'Aphects & DannyVoid'
+
+# flavor text
+print '\ntRaidr ' + current_ver
+print 'Authored By ' + authors
+print '-------------------------------'
+print 'Looking up ' + host_name + '...'
+
+# retreiving json response from tumblr api
+def queryApi(url):
+    req = urllib2.urlopen(url)
+    return json.loads(req.read())
+
+# initial offset value
+offset = 0
+
+# initial url for first query
+url = "http://api.tumblr.com/v2/blog/{host}.tumblr.com/posts?api_key={key}&reblog_info=true".format(
+    host=host_name, key=api_key)
+
+# create directory if it doesn't exist
+if not os.path.exists(os.path.dirname(save_dir)):
+    os.makedirs(os.path.dirname(save_dir))
+
+# initial call the api used to test total posts
+jsonResponse = queryApi(url)
+
+# retrieves total posts
+totalPosts = jsonResponse.get('response', {}).get('total_posts')
+
+# shows total posts
+print 'User found...'
+print 'Looking through ' + str(totalPosts) + ' total posts...'
+
+# duplicate and new image checking
+image_exists = 0
+new_image = 0
+if not args['duplicates']:
+    duplicates_allowed = 20
+else:
+    duplicates_allowed = int(args['duplicates'])
+
+# download all images
+try:
+    while (not (offset >= totalPosts + 20)):
+        url = "http://api.tumblr.com/v2/blog/{host}.tumblr.com/posts?api_key={key}&reblog_info=true&offset={offset}".format(
+            host=host_name, key=api_key, offset=offset)
+        offset += 20;
+        jsonResponse = queryApi(url)
+        posts = jsonResponse.get('response', {}).get('posts', {})
+        for post in posts:
+            if (args['reblogs'] == 'true'):
+                if ('reblogged_from_id' in post):
+                    photos = post.get('photos', {})
+                    for photo in photos:
+                        pictureUrl = photo.get('original_size', {}).get('url', '')
+                        split = urlparse.urlsplit(pictureUrl)
+                        photoName = save_dir + split.path.split("/")[-1]
+                        if not os.path.isfile(photoName):
+                            urllib.urlretrieve(pictureUrl, photoName)
+                            new_image += 1;
+                            if (new_image == 1):
+                                print 'Found new images...'
+                                print 'Downloading images...'
+                        else:
+                            image_exists += 1;
+            if (args['reblogs'] == 'false'):
+                if ('reblogged_from_id' not in post):
+                    photos = post.get('photos', {})
+                    for photo in photos:
+                        pictureUrl = photo.get('original_size', {}).get('url', '')
+                        split = urlparse.urlsplit(pictureUrl)
+                        photoName = save_dir + split.path.split("/")[-1]
+                        if not os.path.isfile(photoName):
+                            urllib.urlretrieve(pictureUrl, photoName)
+                            new_image += 1;
+                            if (new_image == 1):
+                                print 'Found new images...'
+                                print 'Downloading images...'
+                        else:
+                            image_exists += 1;
+        if (image_exists > duplicates_allowed) and (image_exists < duplicates_allowed + 5):
+            print 'Checking for new images...'
+        if (image_exists >= duplicates_allowed + 5):
+            print 'No new images found...'
+            break
+except Exception: 
+    pass
+
+# create a log
+log = open('rips/tRaidr_log.txt', 'a')
+logging = str(datetime.datetime.now()) + ' | ' + str(host_name) + '.tumblr.com' + ' | ' + str(new_image) + ' images saved' + ' | ' + str(image_exists) + ' images skipped' + ' | ' + 'process took %f seconds' % (time.time() - start_time)
+print >> log, (logging)
+log.close()
+    
+# all done
+print 'All done.'
+print '-------------------------------'
